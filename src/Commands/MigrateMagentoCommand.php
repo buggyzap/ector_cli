e9ab12e3e0c9d92702fc9c6c9675b41fd0e38944
Marco Ingraiti <info@marcoingraiti.it>
2023-06-20 17:42:53 +0200

    refactor: test command with other features

--- src/Commands/MigrateMagentoCommand.php
+++ src/Commands/MigrateMagentoCommand.php
@@ -2,9 +2,12 @@
 
 namespace Ector\Cli\Commands;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
 
 class MigrateMagentoCommand extends Command
 {
@@ -14,7 +17,8 @@ class MigrateMagentoCommand extends Command
     {
         $this
             ->setDescription('Migrate from Magento to Prestashop')
-            ->setHelp('This command migrates data from Magento to PrestaShop.');
+            ->setHelp('This command migrates data from Magento to PrestaShop.')
+            ->addArgument('magento', InputArgument::REQUIRED, 'Magento database name');
 
         // Additional command configuration specific to Magento migration
     }
@@ -24,6 +28,30 @@ class MigrateMagentoCommand extends Command
         $output->writeln('Migrating from Magento to PrestaShop...');
         // Logic for Magento migration
 
-        return Command::SUCCESS;
+        $output->writeLn("Database name: {$input->getArgument('magento')}");
+
+        $section1 = $output->section();
+        $section2 = $output->section();
+
+        $progress1 = new ProgressBar($section1);
+        $progress2 = new ProgressBar($section2);
+
+        $progress1->start(100);
+        $progress2->start(100);
+
+        $i = 0;
+        while (++$i < 100) {
+            $progress1->advance();
+
+            if ($i % 2 === 0) {
+                $progress2->advance(4);
+            }
+
+            usleep(50000);
+        }
+
+        throw new Exception('Non ci siamo');
+
+        return 0;
     }
 }
