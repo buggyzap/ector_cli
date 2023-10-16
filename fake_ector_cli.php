7317167933b57814af65164ca43a83448fd7a0f1
Francesco Mineo <francymin@gmail.com>
2023-10-16 13:35:10 +0200

    refactor(filename): change filename for pre and post install scripts

---
+++ fake_ector_cli.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Ector Cli
+ * 
+ * @author DGCAL SRL <m.ingraiti@dgcal.it>
+ * @version 0.0.1
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+if (file_exists("vendor/autoload.php"))
+    require_once "vendor/autoload.php";
+
+class Ector_cli extends Module
+{
+
+    private $checker;
+
+    public function __construct()
+    {
+        $this->initializeModule();
+        if ($this->checker === null && $this->context->controller instanceof AdminController) {
+            $this->checker = $this->get("ector.checker");
+        }
+    }
+    private function initializeModule()
+    {
+        $this->name = 'ector_cli';
+        $this->tab = 'front_office_features';
+        $this->version = '0.0.1';
+        $this->author = 'DGCAL SRL';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Ector CLI');
+        $this->description = $this->l('CLI to help developers to use helper functions and commands');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+    }
+}
